fe8240a09b1a553f6206ab3d0a3dafa7ee600a7c
joonkeekim <joonkee0701@gmail.com>
2020-08-19 16:59:00 +0900

    8/19

--- 11729.cpp
+++ 11729.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int K;
-vector<vector<int>> v(4);
 vector<pair<int,int>> ans;
 int cnt = 0;
 
@@ -13,16 +12,6 @@ void hanoi(int from, int to, int height)
         return;
 
     hanoi(from, 6 - from - to, height - 1); //전꺼 옮기는 부분
-
-    /*if(height == 1){
-        int tmp = *(v[from].end()-1);
-        v[from].pop_back();
-        v[to].push_back(tmp);
-        return;
-    }*/
-    int tmp = *(v[from].end() - 1);
-    v[from].pop_back();
-    v[to].push_back(tmp);
     cnt += 1;
     ans.push_back({from,to});
     hanoi(6 - from - to, to, height - 1);
@@ -33,11 +22,7 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin >> K;
-    for (int i = 0; i < K; i++)
-    {
-        v[1].push_back(K - i);
-    }
-    //if(v[v[n].size()] == k) 마지막 장 찾음
+    
     hanoi(1, 3, K);
     cout<<cnt<<"\n";
     for(int i = 0 ;i<ans.size();i++){
