a3496618eccdcdf927dab9c29250ba528e80af59
joonkeekim <joonkee0701@gmail.com>
2020-08-20 14:45:00 +0900

    8/20

--- 11729.cpp
+++ 11729.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int K;
 vector<pair<int,int>> ans;
+string s;
 int cnt = 0;
 
 void hanoi(int from, int to, int height)
@@ -13,7 +14,11 @@ void hanoi(int from, int to, int height)
 
     hanoi(from, 6 - from - to, height - 1); //전꺼 옮기는 부분
     cnt += 1;
-    ans.push_back({from,to});
+    //ans.push_back({from,to});
+    s += from + '0';
+    s += ' ';
+    s += to + '0';
+    s += '\n';
     hanoi(6 - from - to, to, height - 1);
 }
 
@@ -21,12 +26,15 @@ int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
     cin >> K;
     
     hanoi(1, 3, K);
     cout<<cnt<<"\n";
-    for(int i = 0 ;i<ans.size();i++){
+    cout<<s;
+    //string으롤 하는게 훨씬 빠르다!!!
+    /*for(int i = 0 ;i<ans.size();i++){
         cout<<ans[i].first<<" "<<ans[i].second<<'\n';
-    }
+    }*/
     return 0;
 }
