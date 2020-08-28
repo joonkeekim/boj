b41130f978719d5519da6f5dfeadf5c0f0afdeba
joonkeekim <joonkee0701@gmail.com>
2020-08-28 14:53:04 +0900

    8/28

--- 2873.cpp
+++ 2873.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
 int arr[1000][1000];
 //홀 홀 이면 문제안됨.
@@ -45,19 +46,19 @@ int main()
         {
             if (i % 2 == 0)
             {
-                for (int j = 0; j < C - 1; j++)
+                for (int j = 0; j < R - 1; j++)
                 {
                     cout << "D";
                 }
             }
             else
             {
-                for (int j = 0; j < C - 1; j++)
+                for (int j = 0; j < R - 1; j++)
                 {
                     cout << "U";
                 }
             }
-            if (i != R - 1)
+            if (i != C - 1)
             {
                 cout << "R";
             }
@@ -68,6 +69,7 @@ int main()
     {
         int Min = 1001;
         pair<int, int> p;
+        string s;
         for (int i = 0; i < R; i++)
         {
             for (int j = 0; j < C; j++)
@@ -84,156 +86,131 @@ int main()
                 }
             }
         }
-        for (int i = 0; i < p.first; i++)
-        {
-            if (i % 2 == 0)
-            {
-                for (int j = 0; j < C - 1; j++)
-                {
-                    cout << "R";
-                }
-            }
-            else
-            {
-                for (int j = 0; j < C - 1; j++)
-                {
-                    cout << "L";
-                }
-            }
-            cout << "D";
-        }
-        ///
-        //홀이면 오른쪽부터
-        //짝이면 왼쪽부터
         if (p.first % 2 != 0)
-        {
-            //무조건 한번은 내려가고
-            int a = p.first;
-            int b = C - 1;
-            bool flag = true;
-            for (int i = 0; i < 2 * C - 1; i++)
-            {
-                if (i == 2 * C - 2)
-                {
-                    cout << "D";
-                    break;
-                }
-                if (flag)
-                {
-                    if (a % 2 == 0)
-                    {
-                        if (b == p.second)
-                        {
-                            cout << "L";
-                            b--;
-                            continue;
-                        }
-                        else
-                        {
-                            cout << "U";
-                            a--;
-                        }
-                    }
-                    else
-                    {
-                        cout << "D";
-                        a++;
-                    }
-                }
-                else
-                {
-                    cout << "L";
-                    b--;
-                }
-                flag = !flag;
-            }
-            for (int i = p.first + 2; i < R; i++)
-            {
-                if (i % 2 != 0)
-                {
-                    for (int j = 0; j < C - 1; j++)
-                    {
-                        cout << "R";
-                    }
-                }
-                else
-                {
-                    for (int j = 0; j < C - 1; j++)
-                    {
-                        cout << "L";
-                    }
-                }
-                if (i != R - 1)
-                {
-                    cout << "D";
-                }
-            }
-        }
-        else
-        {
-            int a = p.first;
-            int b = C - 1;
-            bool flag = true;
-            for (int i = 0; i < 2 * C - 1; i++)
-            {
-                if (i == 2 * C - 2)
-                {
-                    cout << "D";
-                    break;
-                }
-                if (flag)
-                {
-                    if (a % 2 == 0)
-                    {
-                        if (b == p.second)
-                        {
-                            cout << "R";
-                            b++;
-                            continue;
-                        }
-                        else
-                        {
-                            cout << "U";
-                            a--;
-                        }
-                    }
-                    else
-                    {
-                        cout << "D";
-                        a++;
-                    }
-                }
-                else
-                {
-                    cout << "R";
-                    b++;
-                }
-                flag = !flag;
-            }
-            for (int i = p.first + 2; i < R; i++)
+        { //홀인경우
+            for (int i = 0; i < p.first - 1; i++)
             {
                 if (i % 2 == 0)
                 {
                     for (int j = 0; j < C - 1; j++)
                     {
-                        cout << "R";
+                        s += "R";
                     }
                 }
                 else
                 {
                     for (int j = 0; j < C - 1; j++)
                     {
-                        cout << "L";
+                        s += "L";
                     }
                 }
-                if (i != R - 1)
+                s += "D";
+            }
+            int c = 0;
+            for (c; c < p.second; c++)
+            {
+                if (c % 2 == 0)
                 {
-                    cout << "D";
+                    s += "DR";
+                }
+                else
+                {
+                    s += "UR";
+                }
+            }
+            for (c; c < C - 1; c++)
+            {
+                if (c % 2 == 0)
+                {
+                    s += "RD";
+                }
+                else
+                {
+                    s += "RU";
+                }
+            }
+            for (int i = p.first + 1; i < R; i++)
+            {
+                s += "D";
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < C - 1; j++)
+                    {
+                        s += "L";
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < C - 1; j++)
+                    {
+                        s += "R";
+                    }
                 }
             }
         }
-        ///
-        cout << '\n';
+        else
+        {
+            for (int i = 0; i < p.first; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < C - 1; j++)
+                    {
+                        s += "R";
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < C - 1; j++)
+                    {
+                        s += "L";
+                    }
+                }
+                s += "D";
+            }
+            int c = 0;
+            for (c; c < p.second; c++)
+            {
+                if (c % 2 == 0)
+                {
+                    s += "DR";
+                }
+                else
+                {
+                    s += "UR";
+                }
+            }
+            for (c; c < C - 1; c++)
+            {
+                if (c % 2 == 0)
+                {
+                    s += "RD";
+                }
+                else
+                {
+                    s += "RU";
+                }
+            }
+            for (int i = p.first + 2; i < R; i++)
+            {
+                s += "D";
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < C - 1; j++)
+                    {
+                        s += "L";
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < C - 1; j++)
+                    {
+                        s += "R";
+                    }
+                }
+            }
+        }
+        cout << s << "\n";
     }
 
     return 0;
